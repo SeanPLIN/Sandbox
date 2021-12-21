839ad11bd3fd30351b7725f172a16369f234814d
Sean <seanplin@outlook.com>
2021-12-21 12:48:36 +1100

    Window added

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -3,6 +3,17 @@
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
+	sf::Window window(sf::VideoMode(800, 600), "Window");
+
+	while (window.isOpen())
+	{
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			if (evnt.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
 }
