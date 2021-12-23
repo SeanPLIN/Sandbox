94110ddae162fe307bd312cc3d9e78a30c7474e5
Sean <seanplin@outlook.com>
2021-12-23 17:29:33 +1100

    Added ImGui

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "Window");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Window");
+	ImGui::SFML::Init(window);
+
+	sf::Time deltaTime = sf::Time::Zero;
+	sf::Clock frameCounter;
 
 	while (window.isOpen())
 	{
 		sf::Event evnt;
 		while (window.pollEvent(evnt))
 		{
+			ImGui::SFML::ProcessEvent(window, evnt);
 			if (evnt.type == sf::Event::Closed)
 			{
 				window.close();
 			}
 		}
+
+		ImGui::SFML::Update(window, deltaTime);
+		ImGui::Begin("Test");
+		ImGui::End();
+
+		window.clear(sf::Color::Black);
+		ImGui::SFML::Render(window);
+
+		window.display();
+
+		deltaTime = frameCounter.getElapsedTime();
+		frameCounter.restart();
 	}
+
+	ImGui::SFML::Shutdown();
 }
